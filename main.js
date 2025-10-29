28efadc374dc23753e0c5af7724a39aabe68a018
Helber Soares <139668970+helberjf@users.noreply.github.com>
2025-10-29 05:00:59 -0300

    Initialize Mercado Pago SDK in main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// SDK do Mercado Pago
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+// Adicione credenciais
+const client = new MercadoPagoConfig({ accessToken: 'YOUR_ACCESS_TOKEN' });
